50408d921980633636a9a7a27abd5c7e4c9a63d5
Colton Kammes <ckammes@nd.edu>
2021-06-04 10:01:12 -0400

    initial commit

---
+++ screencap.py
@@ -0,0 +1,8 @@
+#!/bin/python3
+
+img = ImageGrab.grab(bbox=(0,0,1920,1080)) #bbox specifies specific region (bbox= x,y,width,height *starts top-left)
+img_np = np.array(img) #this is the array obtained from conversion
+frame = cv2.cvtColor(img_np, cv2.COLOR_BGR2RGB)
+cv2.imshow("test", frame)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
